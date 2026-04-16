d0d20b55df60dbc6cfc5a13a6fe87bf22ec2b35a
Peeves426 <garrettskoch@gmail.com>
2026-04-15 23:38:41 -0500

    beta 1.1

---
+++ games/acornClicker/acornClickScript.js
@@ -0,0 +1,5 @@
+        let acorns = 0;
+        function addAcorn() {
+            acorns += 1;
+            document.getElementById("acornDisplay").innerText = `You have ${acorns} acorns.`
+        }
